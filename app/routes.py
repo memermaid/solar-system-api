d5b58a499bb48425c5a1e865fa5c6d850dec8ad4
Andrea Garcia <andrygzt@gmail.com>
2022-05-04 11:41:18 -0700

    not final version

--- app/routes.py
+++ app/routes.py
@@ -27,12 +27,7 @@ def validate_input(planet_id):
     
 @planets_bp.route('', methods=['GET'])
 def get_all_planets():
-    name_query = request.args.get('name')
-    if name_query:
-        planets = Planet.query.filter_by(name=name_query.capitalize())
-    else:
-        planets = Planet.query.all()
-
+    planets = Planet.query.all()
     planets_response = []
 
     for planet in planets:
@@ -47,15 +42,14 @@ def get_all_planets():
 @planets_bp.route('', methods=['POST'])
 def create_planet():
     request_body = request.get_json()
-
-    new_planet = Planet(name=request_body['name'].capitalize(),
+    new_planet = Planet(name=request_body['name'],
                         description=request_body['description'],
-                        fun_fact=request_body['fun_fact'].capitalize())
+                        fun_fact=request_body['fun_fact'])
     
     db.session.add(new_planet)
     db.session.commit()
 
-    return {'msg': f'Successfuly created a planet {new_planet.name} with id {new_planet.id}'}, 201
+    return {'msg': f'Successfuly created a planet with id {new_planet.id}'}, 201
 
 @planets_bp.route('/<planet_id>',methods = ['GET'])
 def get_one_planet(planet_id):
